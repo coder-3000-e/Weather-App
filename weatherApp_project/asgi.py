45fa0d03fc33e0a0be581a5454c882800bee9be6
Ethan-Coder3000 <95977294+Ethan-Coder3000@users.noreply.github.com>
2023-08-14 21:50:19 +0200

    changes

--- weatherApp_project/asgi.py
+++ weatherApp_project/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for weatherApp project.
+ASGI config for weatherApp_project project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weatherApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weatherApp_project.settings')
 
 application = get_asgi_application()
--- weatherApp_project/wsgi.py
+++ weatherApp_project/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for weatherApp project.
+WSGI config for weatherApp_project project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weatherApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weatherApp_project.settings')
 
 application = get_wsgi_application()

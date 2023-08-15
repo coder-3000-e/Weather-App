2da923169ffe51bc5226cc5de8365a92fa684e33
Ethan-Coder3000 <95977294+Ethan-Coder3000@users.noreply.github.com>
2023-08-15 22:39:44 +0200

    cleanup

--- weatherApp_project/settings.py
+++ weatherApp_project/settings.py
@@ -116,7 +116,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+LOGIN_REDIRECT_URL = '/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static','static_root')
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'pages/static','static_dirs'),
+    )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field

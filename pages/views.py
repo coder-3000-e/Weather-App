572056c061c2fa606cd75976be3e388b1e4854da
Ethan-Coder3000 <95977294+Ethan-Coder3000@users.noreply.github.com>
2023-08-16 13:42:24 +0200

    cleanup

--- pages/views.py
+++ pages/views.py
@@ -2,8 +2,10 @@ import csv
 from io import StringIO
 from django.shortcuts import render
 from django.views.generic import TemplateView
-import json
 import urllib.request
+from weatherApp_project import settings
+
+from weatherApp_project.settings import VISUALCROSSING_API_KEY
 
 def home(request):
     return render(request, 'home.html', {})
@@ -12,8 +14,7 @@ def weather(request):
     if request.method == 'POST':
         city = request.POST['city']
         encoded_city = urllib.parse.quote(city)
-        api_key = ''
-        url = f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{encoded_city}?unitGroup=us&include=days&key={api_key}&contentType=csv'
+        url = f'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/{encoded_city}?unitGroup=uk&include=days&key={VISUALCROSSING_API_KEY}&contentType=csv'
         
         source = urllib.request.urlopen(url).read()
         source = source.decode('utf-8') 
@@ -21,7 +22,7 @@ def weather(request):
         csv_data = StringIO(source) 
         csv_reader = csv.DictReader(csv_data)
         
-        data = {'csv_data': csv_reader}
+        data = {'csv_data': csv_reader, 'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY}
     else:
-        data = {}
+        data = {'google_maps_api_key': settings.GOOGLE_MAPS_API_KEY}
     return render(request, "weather.html", data)
--- weatherApp_project/settings.py
+++ weatherApp_project/settings.py
@@ -129,3 +129,7 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GOOGLE_MAPS_API_KEY = ''
+
+VISUALCROSSING_API_KEY = ''
